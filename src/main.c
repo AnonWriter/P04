ea121fe0ab7c66735ebf4a44674b6fece8574468
Adiel <98379144+AnonWriter@users.noreply.github.com>
2023-11-20 21:51:59 -0600

    contador
    en los cambios, use un for para contar los numeros del 1 al 10

--- src/main.c
+++ src/main.c
@@ -5,5 +5,10 @@ int main(int argn, char *args[])
     // Esta es la funcion principal
     printf("Hola mundo.\n");
 
+    for(int i = 0; i < 10; i++){
+        printf("%d", i + 1);
+    }
+    printf("\n");
+
     return 0;
 }
