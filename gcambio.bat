a9833834a09cc7b1a4c1ed61764f7c246d63553a
AnonWriter <esc.anonimo.1@gmail.com>
2023-11-20 22:05:33 -0600

    video

---
+++ gcambio.bat
@@ -0,0 +1,4 @@
+Comandos para realizar los cambios y reflejarlos en el repositorio.
+git add .
+git commit -m ""
+git push -u origin main
---
+++ gclone.bat
@@ -0,0 +1,1 @@
+git clone https://github.com/AnonWriter/P04.git
