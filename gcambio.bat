8292992830a43304687d9cbdce29752da5ab9ed7
AnonWriter <esc.anonimo.1@gmail.com>
2023-11-25 13:07:29 -0600

    elimine tres archivos

--- gcambio.bat
+++
@@ -1,4 +0,0 @@
-Comandos para realizar los cambios y reflejarlos en el repositorio.
-git add .
-git commit -m ""
-git push -u origin main
--- gclone.bat
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/AnonWriter/P04.git
